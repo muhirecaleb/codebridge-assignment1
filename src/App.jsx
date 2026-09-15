8cdde279693e09558108f7f3fdca2e16b22436d6
caleb <muhirecaleb1@gmail.com>
2026-09-15 12:11:58 +0200

    feat: replace Blog component with Post component and update routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Blog from "./pages/Blog";
+import Post from "./pages/Post";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/blog" element={<Blog />} />
+      <Route path="/blog/*" element={<Post />} />
     </Routes>
   );
 }
